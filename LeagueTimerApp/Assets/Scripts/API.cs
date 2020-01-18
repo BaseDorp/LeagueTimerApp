c8efe883718bb56827ec0a9a0dc3e7285f68a795
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-01-18 01:46:27 -0600

    Added class to pull from api
    added a class to pull from riot api. doesnt really work though

---
+++ LeagueTimerApp/Assets/Scripts/API.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    private const string ChampionEndpoint = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+    private const string APIKey = "RGAPI-f396e752-33a4-46ff-a8d5-8b12f4b04f24";
+
+    public void Request()
+    {
+        WWWForm form = new WWWForm();
+
+        Dictionary<string, string> headers = form.headers;
+        headers["X-Riot-Token"] = APIKey;
+
+        WWW request = new WWW(ChampionEndpoint, null, headers);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+    }
+}
---
+++ LeagueTimerApp/Assets/Scripts/SpellTimers.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpellTimers : MonoBehaviour
+{
+    Image spellImage;
+    Button button;
+    [SerializeField]
+    TextMeshProUGUI Text;
+
+    int flashTime = 300;
+    int igniteTime = 180;
+    int teleportTime = 360;
+    int healTime = 240;
+    int barrierTime = 180;
+    int cleanseTime = 210;
+    int exhaustTime = 210;
+    int ghostTime = 180;
+    int smiteTime = 15;
+    float currentTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spellImage = GetComponent<Image>();
+        button = GetComponent<Button>();
+        //Text = GetComponent<TextMeshProUGUI>();
+
+        Text.text = " ";
+    }
+
+    
+    void Update()
+    {
+        button.onClick.AddListener(TaskOnClick);
+
+        // Converts the current number for the timer to Text
+        this.Text.text = Mathf.RoundToInt(this.currentTime).ToString();
+
+        // Decreases time if a timer has started
+        if (this.currentTime > 0)
+        {
+            this.currentTime -= Time.deltaTime;
+        }
+
+        // Clears the number once the timer reaches 0
+        if (this.currentTime <= 0)
+        {
+            this.Text.text = " ";
+        }
+    }
+
+    
+    void TaskOnClick()
+    {
+        // Changes color of button to show it's been pressed
+        this.spellImage.color = new Color(25,25,25);
+
+        // Starts Timer when button is pressed
+        switch (this.spellImage.sprite.name)
+        {
+            case "Flash":
+                this.currentTime = flashTime;
+                break;
+            case "Teleport":
+                this.currentTime = teleportTime;
+                break;
+            case "Smite":
+                this.currentTime = smiteTime;
+                break;
+            case "Heal":
+                this.currentTime = healTime;
+                break;
+            case "Ignite":
+                this.currentTime = igniteTime;
+                break;
+            case "Ghost":
+                this.currentTime = ghostTime;
+                break;
+            case "Barrier":
+                this.currentTime = barrierTime;
+                break;
+            case "Exhaust":
+                this.currentTime = exhaustTime;
+                break;
+            case "Cleanse":
+                this.currentTime = cleanseTime;
+                break;
+        }
+    }
+}
